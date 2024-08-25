bfc47915f349950761ff9f5f2bee97b52666eb42
hanyangclarence <mahlerrrr76@gmail.com>
2024-08-25 18:49:22 +0800

    update

--- yolo_finetune/train_test_split.py
+++ yolo_finetune/train_test_split.py
@@ -6,17 +6,24 @@ train_ratio = 0.9
 
 all_images = os.listdir(os.path.join(data_dir, 'images'))
 
-train_images = random.sample(all_images, int(len(all_images) * train_ratio))
-val_images = list(set(all_images) - set(train_images))
+
+# random split
+# train_images = random.sample(all_images, int(len(all_images) * train_ratio))
+# val_images = list(set(all_images) - set(train_images))
+
+# split by scene id
+train_images = [name for name in all_images if int(name.split('--')[1].split('-')[0]) < 800]
+val_images = [name for name in all_images if int(name.split('--')[1].split('-')[0]) >= 800]
+
 
 print(f"Total images: {len(all_images)}")
 print(f"Train images: {len(train_images)}")
 print(f"Validation images: {len(val_images)}")
 
-with open(os.path.join(data_dir, 'train.txt'), 'w') as f:
+with open(os.path.join(data_dir, 'train_x.txt'), 'w') as f:
     for image in train_images:
         f.write(f"./images/{image}\n")
 
-with open(os.path.join(data_dir, 'val.txt'), 'w') as f:
+with open(os.path.join(data_dir, 'val_x.txt'), 'w') as f:
     for image in val_images:
         f.write(f"./images/{image}\n")

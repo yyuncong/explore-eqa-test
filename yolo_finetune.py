c9429369b9e7f056990fb98d88bf1ffa14ed145c
hanyangclarence <mahlerrrr76@gmail.com>
2024-08-25 18:55:08 +0800

    update

--- yolo_finetune.py
+++ yolo_finetune.py
@@ -1,8 +1,28 @@
 from ultralytics import YOLOWorld
 
+
+
+# start from scratch
 # Load a pretrained YOLOv8s-worldv2 model
 model = YOLOWorld("yolov8x-worldv2.pt")
-
 # Train the model on the COCO8 example dataset for 100 epochs
-results = model.train(data="/gpfs/u/home/LMCG/LMCGhazh/scratch/yanghan/explore-eqa-test/yolo_finetune/dataset.yaml", epochs=100, imgsz=1280, batch=8 * 6, device=[0, 1, 2, 3, 4, 5])
+results = model.train(
+    data="/gpfs/u/home/LMCG/LMCGhazh/scratch/yanghan/explore-eqa-test/yolo_finetune/dataset.yaml",
+    epochs=100,
+    imgsz=1280,
+    batch=8 * 6,
+    device=[0, 1, 2, 3, 4, 5]
+)
 
+
+
+# # resume
+# model = YOLOWorld("/gpfs/u/home/LMCG/LMCGnngn/scratch/yanghan/3d/explore-eqa-test/runs/detect/train2/weights/last.pt")
+# results = model.train(
+#     data="/gpfs/u/home/LMCG/LMCGhazh/scratch/yanghan/explore-eqa-test/yolo_finetune/dataset.yaml",
+#     epochs=100,
+#     imgsz=1280,
+#     batch=8 * 6,
+#     device=[0, 1, 2, 3, 4, 5],
+#     resume=True,
+# )
